ccf158e0d5927e7e15054b4819e57018129029f0
John Moreno <jmorenoal@unal.edu.co>
2026-07-10 17:40:00 -0500

    feat(docker): agrega .env.local y modificar CORS

--- UNDesparche/backend-undesparche/undesparche/settings.py
+++ UNDesparche/backend-undesparche/undesparche/settings.py
@@ -151,7 +151,4 @@ FIREBASE_CREDENTIALS_PATH = BASE_DIR / "firebase_credentials.json"
 FIREBASE_STORAGE_BUCKET = os.getenv("FIREBASE_STORAGE_BUCKET")
 
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
-    "http://127.0.0.1:5173",
-]
+CORS_ALLOWED_ORIGINS = os.getenv("CORS_ALLOWED_ORIGINS", "").split(",")  # Lista de orígenes permitidos para solicitudes CORS, obtenida de la variable de entorno.
